bc909c0a1f6f1e06f750fab9de0672389ed1a48e
josephrajp <mailjosephraj@gmail.com>
2016-12-19 06:23:02 +0530

    Completed Sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll('.feature-item', '85%');
 new RevealOnScroll('.testimonial', '70%');
+var stickyHeader = new StickyHeader();
